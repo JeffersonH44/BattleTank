285c891cc59fddf2045b165c848b2aea63cab048
Jefferson Javier Hernández Panqueba <jeffersonh44@gmail.com>
2018-08-22 17:35:10 -0500

    AI aiming at player target

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+#include "../Public/TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto TankController = GetControlledTank();
+	if (!TankController)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No AI tank controller!!!"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing: %s"), *(TankController->GetName()))
+	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Aiming at: %s"), *(PlayerTank->GetName()))
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());;
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerController = GetWorld()->GetFirstPlayerController();
+	if (!PlayerController) 
+	{
+		UE_LOG(LogTemp, Error, TEXT("No player controller found from: %s"), *(GetControlledTank()->GetName()))
+		return nullptr;
+	} 
+
+	return Cast<ATank>(PlayerController->GetPawn());
+}
+
+
---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+	auto TankController = GetControlledTank();
+	if (!TankController) 
+	{
+		UE_LOG(LogTemp, Error, TEXT("No tank controller!!!"))
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController possessing: %s"), *(TankController->GetName()))
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());;
+}
+
+
